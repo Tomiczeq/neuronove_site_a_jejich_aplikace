0afa91cfba1636cb91e4f36bd0abff936e8c8057
Tomas Kratochvil <tomas.kratochvil@firma.seznam.cz>
2020-07-18 00:18:33 +0000

    fix

--- final_eval/final_evaluate_conv_rnn.py
+++ final_eval/final_evaluate_conv_rnn.py
@@ -93,7 +93,6 @@ def main():
     datapath = args.datapath
     modelpath = args.modelpath
     word_vectors_path = args.word_vectors_path
-    word_vectors_dim = args.word_vectors_dim
 
     with open(datapath, 'r') as f:
         final_data = json.load(f)
